57070688aa3935d2a305cfd8365c7ce9ed3ed3f1
rehankhan21 <narutop224@gmail.com>
2023-03-29 01:50:38 -0400

    EvenOrOdd main file

--- EvenOrOdd/main.go
+++ EvenOrOdd/main.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+
+	for _, num := range numbers {
+		if num%2 == 0 {
+			fmt.Println(num, " is Even")
+		}else {
+			fmt.Println(num, " is Odd")
+		}
+	}
+}
