271dfd627ff7b8a29b741ea7c78ec9788619b57a
rehankhan21 <narutop224@gmail.com>
2023-03-31 00:55:47 -0400

    iterated through map

--- Map/main.go
+++ Map/main.go
@@ -4,19 +4,23 @@ import "fmt"
 
 func main() {
 	// creating a map where all keys are type string and all values are type string as well
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"greem": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"greem": "#4bf745",
+	}
 
 	// another way to make a map
 	//var colors map[string]string
 
-	colors := make(map[int]string)
+	// colors := make(map[int]string)
 
-	colors[10] = "#ffffff"
-	
-		delete(colors, 10)
+	// colors[10] = "#ffffff"
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string)  {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is",  hex)
+	}
 }
