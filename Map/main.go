7072e5786847570c6fb9aeb332a9b4ffadfb211b
rehankhan21 <narutop224@gmail.com>
2023-03-31 00:50:33 -0400

    multiple ways to make a map plus delteing elements from the map

--- Map/main.go
+++ Map/main.go
@@ -4,10 +4,19 @@ import "fmt"
 
 func main() {
 	// creating a map where all keys are type string and all values are type string as well
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"greem": "#4bf745",
-	}
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"greem": "#4bf745",
+	// }
+
+	// another way to make a map
+	//var colors map[string]string
+
+	colors := make(map[int]string)
+
+	colors[10] = "#ffffff"
+	
+		delete(colors, 10)
 
 	fmt.Println(colors)
 }
